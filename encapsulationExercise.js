25d5f7ade0e4d7816d4bec31dfef7190ebbb8117
ashutester <ashubapat@gmail.com>
2025-05-24 13:26:48 -0700

    Test first

---
+++ encapsulationExercise.js
@@ -0,0 +1,22 @@
+class Employee{
+    getEmpId(){
+        return this.empId;
+    }
+    getEmpName(){
+        return this.empName
+    }
+    getPhoneNumber(){
+        return this.phoneNumber;
+    }
+    setEmpDetails(empId,empName,phoneNumber){
+        this.empId = empId;
+        this.empName = empName;
+        this.phoneNumber = phoneNumber;
+    }
+}
+
+let ashutosh = new Employee();
+ashutosh.setEmpDetails(1001,'Ashutosh Bapat','6048180724');
+console.log(ashutosh.getEmpId());
+console.log(ashutosh.getEmpName());
+console.log(ashutosh.getPhoneNumber());
